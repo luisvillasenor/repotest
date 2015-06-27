e1986e0b5578fbf8dd26b8a8f520ac708b2d8d0c
== <=>
2015-06-27 16:00:39 -0500

    otro cambio desde el master

--- test.rb
+++ test.rb
@@ -2,4 +2,7 @@ Para iniciar un proyecto en Git de forma local primero posicionate en el directo
 Comando para generar el proyecto:
 git init <nom_proy>
 despues has un cd <nom_proy> para cambiarte al directorio del proyecto nuevo donde haras todo el desarrollo de tu aplicacion.
+ESTA LINEA SE HACE DESDE MASTER y tuvimos que cambiarnos primero con 
+el comando git checkout master
+ya que estabamos en el branch testing.
 
