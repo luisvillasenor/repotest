f73884925675dc99c682c0082514a738ac089edc
== <=>
2015-06-27 15:33:16 -0500

    confirmacion inicial del proyecto

---
+++ test.rb
@@ -0,0 +1,5 @@
+Para iniciar un proyecto en Git de forma local primero posicionate en el directorio de trabajo que tu quieras. por ejemplo en tu htdocs seria una buena opcion dado que ahi correra tu aplicacion en modo desarrollo.
+Comando para generar el proyecto:
+git init <nom_proy>
+despues has un cd <nom_proy> para cambiarte al directorio del proyecto nuevo donde haras todo el desarrollo de tu aplicacion.
+
